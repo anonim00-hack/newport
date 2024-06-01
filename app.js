ad8f54d25719a47fe3c84402f63b34c73f3447a7
anonim00-hack <m65334287@gmail.com>
2024-06-01 13:59:00 +0500

    add

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const pass = document.querySelector('.inp-pass');
 const Name = document.querySelector('.inp-name');
 const apply = document.querySelector('.apply');
+const news = document.querySelector('.news');
+const currency = document.querySelector('.currency');
 const repeat = document.querySelector('.rep-button');
+
+const iframe1 = document.querySelector('.iframe1');
+const iframe2 = document.querySelector('.iframe2');
 
 const gmailName = document.querySelector('.gmail-name');
 const gmailPass = document.querySelector('.gmail-pass');
 
 const alertBox = document.querySelector('.alert-box');
+const header = document.querySelector('header');
 const body2 = document.querySelector('.body');
+
+
+setInterval(() => {
+    const addBox = document.querySelector('.adds');
+    const adds = document.querySelectorAll('.add');
+    let randomNum = Math.trunc(Math.random() * adds.length); // случайное число от 0 до adds.length - 1
+
+    adds.forEach(add => {
+        add.classList.remove('index'); // Удаляем класс 'index' у всех элементов
+    });
+
+    adds[randomNum].classList.add('index'); // Добавляем класс 'index' к случайному элементу
+}, 20000);
+
 
 // Функция для проверки и отображения body2 и alertBox
 function checkAndDisplay() {
@@ -21,9 +41,11 @@ function checkAndDisplay() {
     }
 
     if (savedName && savedPass) {
+        header.style.display = 'block';
         body2.style.display = 'grid';
         alertBox.style.display = 'none';
     } else {
+        header.style.display = 'none';
         body2.style.display = 'none';
         alertBox.style.display = 'block';
     }
@@ -45,6 +67,7 @@ apply.addEventListener('click', () => {
         gmailPass.textContent = pass.value;
 
         // Показываем body2 и скрываем alertBox
+        header.style.display = 'block';
         body2.style.display = 'grid';
         alertBox.style.display = 'none';
     }
@@ -63,6 +86,7 @@ repeat.addEventListener('click', () => {
     localStorage.removeItem('savedPass');
     gmailName.textContent = '';
     gmailPass.textContent = '';
+    header.style.display = 'none';
     body2.style.display = 'none';
     alertBox.style.display = 'block';
 });
@@ -78,3 +102,13 @@ Pin.addEventListener('input', () => {
         wall.classList.remove('unlock');
     }
 });
+
+currency.addEventListener('click', () => {
+    iframe1.style.display = 'none';
+    iframe2.style.display = 'block';
+})
+
+news.addEventListener('click', () => {
+    iframe1.style.display = 'block';
+    iframe2.style.display = 'none';
+})
